696e3fb68e8691f6286471adff46ade46ffb425f
jeeyeon <jeeyeon118@gmail.com>
2023-10-15 21:36:46 +0900

    20231015

--- src/com/step5/DeptManager.java
+++ src/com/step5/DeptManager.java
@@ -9,6 +9,7 @@ import java.util.Vector;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -81,7 +82,7 @@ public class DeptManager extends JFrame implements ActionListener {
 		if(obj == jbtnSelect) {
 			System.out.println("조회버튼 클릭");
 			while(dtm_dept.getRowCount()>0) {       //테이블의 줄이 0 이상이면(일단 출력이 된 화면이라면)
-				dtm_dept.removeRow(0);                 //0번째 로우 삭제!
+				dtm_dept.removeRow(0);              //0번째 로우 삭제!
 			}
 			for(int i=0; i<deptList.size();i++) {
 				DeptDTO rdept = deptList.get(i);
@@ -94,6 +95,84 @@ public class DeptManager extends JFrame implements ActionListener {
 				//addRow 위에 삽입된 v를 table 맨 아래줄로 추가함. 
 				dtm_dept.addRow(v);
 			}
+		}
+		/*삭제하기*/
+		else if(obj == jbtnDelete) {
+			System.out.println("삭제버튼 클릭");
+			int index = jt_dept.getSelectedRow(); //index는 사용자가 선택한 로우의 번호
+			//index가 0보다 작다(선택안되어있을 때)
+			if(index<0) {
+				//JOptionPane 특정상황 시 팝업 창 띄우기
+				//shouMessageDialog(클래스, 메시지, 타이틀명, 아이콘) 메시지 띄우는 메소드
+				JOptionPane.showMessageDialog(this, "삭제할 데이터를 선택하시오", "Info", JOptionPane.INFORMATION_MESSAGE);
+				return;
+			}
+			else {//index가 선택되었을 때
+				System.out.println(index);//선택한 index의 번호는?
+				DeptDTO rdept = deptList.remove(index); //DTo의 deptList에서 해당 index 삭제(remove)
+				System.out.println(rdept+", "+rdept.getDeptno()+", "+rdept.getDname()+", "+rdept.getLoc());//삭제된 것 불어오기 
+				//삭제 성공했니??(이런 조건들이 빈틈없는 코드를 완성함!!
+				if(rdept!=null) {//삭제된 rdept가 null값이 아니니? 그럼 삭제된거
+					JOptionPane.showMessageDialog(this, "삭제 성공 하였습니다.", "Info", JOptionPane.INFORMATION_MESSAGE);
+					refreshData(); //새로고침 메소드 실행
+				}
+			}
+		}
+		/*종료 하기*/
+		else if(obj == jbtnExit) {
+			System.out.println("종료버튼 클릭");
+			dispose();
+		}
+		/*행추가 하기*/
+		else if(obj == jbtnAdd) {
+			System.out.println("행추가버튼 클릭");
+			Vector<Object> addRow = new Vector<>();
+			dtm_dept.addRow(addRow);
+		}
+		/*행삭제 하기 1*/
+		else if(obj == jbtnDel) {
+			System.out.println("행삭제버튼 클릭");
+			//행이 없을 때 멈추려면?
+			int index = dtm_dept.getRowCount();
+		    if (index > 0) {
+		        dtm_dept.removeRow(0);
+		        JOptionPane.showMessageDialog(this, "행 삭제 성공하였습니다.", "Info", JOptionPane.INFORMATION_MESSAGE);
+		    } else {
+		        JOptionPane.showMessageDialog(this, "삭제할 행이 없습니다.", "Info", JOptionPane.INFORMATION_MESSAGE);
+		    }
+		}
+		/*행삭제 하기 2*/
+		else if(obj == jbtnDel) {
+			System.out.println("행삭제버튼 클릭");
+			int index = jt_dept.getRowCount();
+			if(index>0) {
+				DefaultTableModel model = (DefaultTableModel) jt_dept.getModel();
+				model.removeRow(0);
+				JOptionPane.showMessageDialog(this, "행 삭제 성공하였습니다.", "Info", JOptionPane.INFORMATION_MESSAGE);
+			}
+			else {
+				JOptionPane.showMessageDialog(this, "삭제할 행이 없습니다.", "Info", JOptionPane.INFORMATION_MESSAGE);
+			}
+		}
+		/*행삭제 하기 2*/
+		else if(obj == jbtnDel) {
+			System.out.println("행삭제버튼 클릭");
+				
+		}
+		
+		/*입력 하기*/
+		else if(obj == jbtnInsert){
+			System.out.println("입력버튼 클릭");
+			//매개변수로 전달된 벡터(Vector) 형태의 데이터를 테이블에 새로운 행으로 추가합니다.
+			List<Object> addRow = new Vector<>();
+			//?는 와일드카드 타입으로, 어떤 타입도 될 수 있다는 의미->유연성 확보, 이를 메소드에 전달하여 새로운 행 추가
+			dtm_dept.addRow((Vector<?>) addRow);
+		}
+	}
+	//새로고침 메소드
+	public void refreshData() { //테이블이 9 이상이면 0번째 로우 삭제하기
+		while(dtm_dept.getRowCount()>0) {
+			dtm_dept.removeRow(0);
 		}
 	}
 
