a17b7afa1132c900674609a87425024dd2f91959
jeeyeon <jeeyeon118@gmail.com>
2023-10-21 21:34:17 +0900

    20231021-7

--- src/jdbc/oracle/BookVO.java
+++ src/jdbc/oracle/BookVO.java
@@ -20,38 +20,28 @@ public class BookVO {
 	public int getB_no() {
 		return b_no;
 	}
-
 	public void setB_no(int b_no) {
 		this.b_no = b_no;
 	}
-
 	public String getB_title() {
 		return b_title;
 	}
-
 	public void setB_title(String b_title) {
 		this.b_title = b_title;
 	}
-
 	public String getB_author() {
 		return b_author;
 	}
-
 	public void setB_author(String b_author) {
 		this.b_author = b_author;
 	}
-
 	public int getB_price() {
 		return b_price;
 	}
-
 	public void setB_price(int b_price) {
 		this.b_price = b_price;
 	}
-
 	/*메인메소드*/
 	public static void main(String[] args) {
-
 	}
-
 }
