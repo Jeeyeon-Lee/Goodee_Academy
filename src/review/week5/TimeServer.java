39f2e0de1de00fb8e185473c70345ae9263d18de
jeeyeon <jeeyeon118@gmail.com>
2023-10-21 20:00:39 +0900

    20231021

---
+++ src/review/week5/TimeServer.java
@@ -0,0 +1,15 @@
+package review.week5;
+
+import java.net.Socket;
+
+public class TimeServer {
+	/*선언부*/
+	Socket client = null;
+	/*생성자*/
+	public TimeServer(Socket client) {
+		this.client = client;
+	}
+	/*정의메소드*/
+
+	/*메인메소드*/
+}
